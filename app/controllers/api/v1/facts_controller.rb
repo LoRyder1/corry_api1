7c897cd44d26ce4110805336a83d74608919a8c7
Lovinder Pnag <lspnag@gmail.com>
2020-08-29 22:27:56 -0500

    using sonix api

--- app/controllers/api/v1/facts_controller.rb
+++ app/controllers/api/v1/facts_controller.rb
@@ -1,18 +1,167 @@
+require 'net/http'
+require 'uri'
+
+
 class Api::V1::FactsController < ApplicationController
   before_action :find_fact, only: [:show, :update, :destroy]
 
-  # GET /users
+
   def index
     @facts = Fact.all
-    render json: @facts
+    # render json: @facts
+    
+
+
+    # p 'Hello World!'
+
+    $API_KEY = "V0lOejNUz5rx2ITKPf8DUgtt"
+
+
+    # require 'net/http'
+
+    # uri = URI.parse("http://example.org")
+
+    # Shortcut
+    # response = Net::HTTP.post_form(uri, {"user[name]" => "testusername", "user[email]" => "testemail@yahoo.com"})
+
+    # Full control
+    # http = Net::HTTP.new(uri.host, uri.port)
+    # request = Net::HTTP::Post.new(uri.request_uri)
+      # request.set_form_data({"user[name]" => "testusername", "user[email]" => "testemail@yahoo.com"})
+    # response = http.request(request)
+      # raise response
+    # render :json => response.body
+
+
+    # curl -XGET https://api.sonix.ai/v1/media?page=0 -H "Authorization: Bearer abcd"
+    
+    # uri = URI.parse("https://api.sonix.ai/v1/media?page=0")
+    # request = Net::HTTP::Get.new(uri)
+    # request["Authorization"] = "Bearer V0lOejNUz5rx2ITKPf8DUgtt"
+
+    # req_options = {
+    #   use_ssl: uri.scheme == "https",
+    # }
+
+    # response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    #   http.request(request)
+    # end
+
+    # response.code
+    # response.body
+
+    # uri = URI.parse("https://api.sonix.ai/v1/media?page=0 -H 'Authorization: Bearer V0lOejNUz5rx2ITKPf8DUgtt'")
+    # http = Net::HTTP.new(uri.host, uri.port)
+    # request = Net::HTTP::Post.new(uri.request_uri)
+    # response = http.request(request)
+    # raise response.body
+
+
+    # // Send wav file to sonix, @my_audio.mp3 = absolute path of file
+    # curl -XPOST https://api.sonix.ai/v1/media -H "Authorization: Bearer <API Key>"  \
+    #   -F file=@my_audio.mp3 \
+    #   -F language=en \
+    #   -F name='Aug27_menu' \
+    #   -F keywords='NAS, Pensacola, Galley, menu'
+
+    uri = URI.parse("https://api.sonix.ai/v1/media/j2gojRk2")
+    request = Net::HTTP::Get.new(uri)
+    request["Authorization"] = "Bearer V0lOejNUz5rx2ITKPf8DUgtt"
+    req_options = {
+      use_ssl: uri.scheme == "https",
+      file=@my_audio.wav
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    response.code
+
+
+    uri = URI('http://www.example.com/search.cgi')
+    res = Net::HTTP.post_form(uri, 'q' => ['ruby', 'perl'], 'max' => '50')
+    puts res.body
+
+    uri = URI('http://www.example.com/search.cgi')
+    res = Net::HTTP.post_form(uri, 'q' => ['ruby', 'perl'], 'max' => '50')
+    puts res.body
+
+
+
+
+
+    # curl -XGET https://api.sonix.ai/v1/media/<qmnY1nD5> -H "Authorization: Bearer <API Key>" 
+
+    uri = URI.parse("https://api.sonix.ai/v1/media/j2gojRk2")
+    request = Net::HTTP::Get.new(uri)
+    request["Authorization"] = "Bearer V0lOejNUz5rx2ITKPf8DUgtt"
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    response.code
+
+    # curl -XGET https://api.sonix.ai/v1/media/<qmnY1nD5>/transcript -H "Authorization: Bearer <API Key>" 
+
+    # uri = URI.parse("https://api.sonix.ai/v1/media/j2gojRk2/transcript.json")
+
+    uri = URI.parse("https://api.sonix.ai/v1/media/j2gojRk2/transcript")
+    request = Net::HTTP::Get.new(uri)
+    request["Authorization"] = "Bearer V0lOejNUz5rx2ITKPf8DUgtt"
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end    
+
+    # raise response.body
+
+    render :json => response.body
+
+
+
+
+
+
+
+
+
   end
 
-  # GET /user/:id
+
+
+
+
+
+
+
+
+
+
+
   def show
+
+
+
     render json: @fact
   end
 
-  # POST /users
+
+
+
+
+
+
+
+
+
   def create
     @fact = Fact.new(fact_params)
     if @fact.save
@@ -22,7 +171,7 @@ class Api::V1::FactsController < ApplicationController
     end
   end
 
-  # PUT /users/:id
+
   def update
     if @fact
       @fact.update(fact_params)
@@ -32,7 +181,7 @@ class Api::V1::FactsController < ApplicationController
     end
   end
 
-  # DELETE /users/:id
+
   def destroy
     if @fact
       @fact.destroy
